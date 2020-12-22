2d02a7395a5fbb96b4d409e03a6325405c3091a1
Wen Jun <wenjun.lye@gmail.com>
2020-12-22 23:28:25 +0800

    Set up tags API and Redux slice

---
+++ app/javascript/components/Tags/tagsSlice.tsx
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { readTags, createTag, updateTag, deleteTag } from "../../../resources/api/tags"
+import { SUCCESS } from '../sliceUtils';
+
+// thunks
+export const readTagsThunk = createAsyncThunk("tags/readTags", readTags);
+export const createTagThunk = createAsyncThunk("tags/createTag", createTag);
+export const updateTagThunk = createAsyncThunk("tags/updateTag", updateTag);
+export const deleteTagThunk = createAsyncThunk("tags/deleteTag", deleteTag);
+
+export const tagSlice = createSlice({
+  name: "tags",
+  initialState: {
+    data: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: builder => {
+    // create tag
+    builder.addCase(createTagThunk.fulfilled, (state, action) => {
+      state.status = SUCCESS;
+      state.data.push(action.payload);
+    })
+
+    // read tags
+    builder.addCase(readTagsThunk.fulfilled, (state, action) => {
+      state.status = SUCCESS;
+      state.data = state.data.concat(action.payload);
+    })
+
+    // update tag
+    builder.addCase(updateTagThunk.fulfilled, (state, action) => {
+      state.status = SUCCESS;
+      const i = state.data.findIndex(task => task.id === action.payload.id);
+      state.data[i] = action.payload;
+    })
+
+    // delete tag
+    builder.addCase(deleteTagThunk.fulfilled, (state, action) => {
+      state.status = SUCCESS;
+      const i = state.data.findIndex(task => task.id === action.meta.arg);
+      state.data.splice(i, 1);
+    })
+  }
+})
+
+// selectors
+export const tagsSelector = state => state.tags.data;
+export const tagsStatusSelector = state => state.tags.status;
+
+// reducer
+export default tagSlice.reducer;
