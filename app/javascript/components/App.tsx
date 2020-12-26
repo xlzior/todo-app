1edc1d635b3be78325c0f0cfe5c25bd6e579ef9a
Wen Jun <wenjun.lye@gmail.com>
2020-12-26 15:43:57 +0800

    Rearrange file structure
    Rename App to Routes
    Rename index to App
    Move task-related CSS back into Task folder

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,20 +1,39 @@
 import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
 
-import Tasks from '.';
+import TaskList from './Tasks/TaskList';
+import { taskStatusSelector, readTasksThunk } from './Tasks/tasksSlice';
+import { tagsStatusSelector, readTagsThunk } from './Tags/tagsSlice';
+
 import TagsList from './Tags/TagsList';
 
-export default function App() {
+interface Task {
+  task: string,
+  completed: boolean
+}
+
+export default function Tasks() {
+  const dispatch = useDispatch();
+  const taskStatus = useSelector(taskStatusSelector);
+  const tagStatus = useSelector(tagsStatusSelector);
+
+  React.useEffect(() => { // tasks
+    if (taskStatus === 'idle') dispatch(readTasksThunk());
+  }, [taskStatus, dispatch]);
+
+  React.useEffect(() => { // tags
+    if (tagStatus === 'idle') dispatch(readTagsThunk());
+  }, [tagStatus, dispatch]);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags"><TagsList /></Route>
-        <Route exact path="/"><Tasks /></Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <div className="app-title">
+        <h1>Tasks</h1>
+      </div>
+      <main>
+        <TaskList />
+        <TagsList />
+      </main>
+    </div>
   )
 }
