a9730f5cc763ac68edf1e1fa5ad5678aed2182d8
Wen Jun <wenjun.lye@gmail.com>
2020-12-27 21:01:46 +0800

    Close tags popup menu when mouse clicks outside

--- app/javascript/components/Tags/TagsSelect/index.tsx
+++ app/javascript/components/Tags/TagsSelect/index.tsx
@@ -8,7 +8,24 @@ const isTagRelevant = (name, searchTerm) => {
   return name.toLowerCase().includes(searchTerm.toLowerCase());
 }
 
-export default function TagsSelect({ isTagsOpen, selection, setSelection }) {
+function useOnClickOutside(ref, fn, deps) {
+  React.useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        fn();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, ...deps]);
+}
+
+export default function TagsSelect({ isTagsOpen, closeTags, selection, setSelection }) {
+  const wrapperRef = React.useRef(null);
+  useOnClickOutside(wrapperRef, closeTags, [selection]);
   const allTags = useSelector(tagsSelector);
   const [searchTerm, setSearchTerm] = React.useState("");
 
@@ -17,8 +34,7 @@ export default function TagsSelect({ isTagsOpen, selection, setSelection }) {
     : allTags.filter(tag => isTagRelevant(tag.attributes.name, searchTerm));
 
   const updateSelection = (event, id) => {
-    const newValue = event.target.checked;
-    if (newValue) {
+    if (event.target.checked) {
       setSelection(prev => prev.concat([{ id, type: "tags" }]));
     } else {
       setSelection(prev => prev.filter(elem => elem.id !== id));
@@ -26,7 +42,7 @@ export default function TagsSelect({ isTagsOpen, selection, setSelection }) {
   }
 
   return isTagsOpen ? (
-    <div className="tags-select">
+    <div className="tags-select" ref={wrapperRef}>
       <input
         className="tag-search"
         type="text"
