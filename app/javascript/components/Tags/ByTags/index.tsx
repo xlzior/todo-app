b0ef5137a95ec029e1d25847afff6ebb30888b6f
Wen Jun <wenjun.lye@gmail.com>
2020-12-28 11:22:58 +0800

    Implement ByTags to view list of tasks with a certain tag

--- app/javascript/components/Tags/ByTags/index.tsx
+++ app/javascript/components/Tags/ByTags/index.tsx
@@ -3,15 +3,19 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import Page from '../../Page';
+import TaskList from '../../Tasks/TaskList';
+import useDataFiller from '../../Tasks/useDataFiller';
 import { tagSelector } from '../tagsSlice';
 import './index.css';
 
 export default function ByTags() {
   const { id } = useParams();
   const tagData = useSelector(tagSelector(id));
+  const rawTasks = tagData?.relationships.tasks.data;
+  const filledTasks = useDataFiller(rawTasks);
   return (
     <Page title={tagData?.attributes.name}>
-      <p>Hi</p>
+      <TaskList tasks={filledTasks} />
     </Page>
   );
 }
---
+++ app/javascript/components/Tasks/useDataFiller.tsx
@@ -0,0 +1,7 @@
+import { useSelector } from 'react-redux';
+import { tasksSelector } from "./tasksSlice";
+
+export default function useDataFiller(rawTasks = []) {
+  const allTasks = useSelector(tasksSelector);
+  return rawTasks.map(({ id }) => allTasks.find(elem => elem.id === id));
+}
